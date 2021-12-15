bc43473f94d7376711737718b8c816d41ee3e0c9
lailahadaya <78495286+lailahadaya@users.noreply.github.com>
2021-12-15 14:52:48 +0000

    Add files via upload

---
+++ Hadaya_SNF_pipeline_code.R
@@ -0,0 +1,221 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# Main pipeline.
+
+# Study title: "Characterising distinct behavioural subgroups of very preterm born children and 
+#   identifying differences in neonatal structural and functional brain patterns" 
+# Date: 19/11/2021
+# Authors: L Hadaya, K Dimitrakopoulou, L Vanes, D Kanel, S Fenn-Moltu, D Pecheva, G Ball, AD Edwards, 
+# SJ Counsell, M Saqi, D Batalle and C Nosarti
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
+
+### ==== Install Packages ====
+install.packages("SNFtool")
+install.packages("readxl")
+install.packages("openxlsx")
+install.packages("BiocManager")
+BiocManager::install("ConsensusClusterPlus")
+install.packages("cluster")
+BiocManager::install("CancerSubtypes")
+install.packages("purrr")
+install.packages("ggplot2")
+install.packages("gdata")
+install.packages("diceR")
+
+### ==== Load libraries ====
+library(SNFtool)
+library(readxl)
+library(openxlsx)
+library(ConsensusClusterPlus)
+library(cluster)
+library(CancerSubtypes)
+library(data.table)
+library(purrr)
+library(gdata)
+library(ggplot2)
+library(diceR)
+library(alluvial)
+library(dplyr)
+library(ggplot2)
+
+### ==== I. Load .RData for selected data types + sample ====
+
+load("data_types.RData") # load your data
+df_data_type_1 <-  df_ex_func # first data type - all numeric variables
+df_data_type_2 <- df_socio_emo # second data type - all numeric variables
+df_data_type_3 <- df_clin_social_risk # third data type - mixed data type
+
+### ==== II. Cluster number estimation table creation ====
+#For each combination of SNF hyperparameters K={10, 15, 20, 25, 30} & alpha={0.3,, 0.4, 0.5, 0.6, 0.7, 0.8} 
+#Calculate best and second best C based on eigengap using, estimateNumberOfClustersGivenGraph()
+#Choose the cluster numbers C that most combinations agree on and are clinically relevant.
+
+# Set param combinations
+N = dim(df_data_type_1)[1]
+N #number of subjects
+parameter_combos_K <- c(rep(10,6), rep(15,6), rep(round((N/10), digits=0),6),rep(25,6), rep(30,6))
+parameter_combos_alpha <- c(rep(c(0.3, 0.4, 0.5, 0.6, 0.7, 0.8),5))
+
+param_combos <- data.frame(c(1:length(parameter_combos_alpha)), parameter_combos_K, parameter_combos_alpha)
+colnames(param_combos) <- c("combo_Num", "K", "alpha")
+
+# Get Cluster Estimations for all 30 combination runs:
+estClustNumb_paramCombos <- data.frame()
+for(i in 1:dim(param_combos)[1]){
+  paste("K =", param_combos[i,2], ", alpha =", param_combos[i,3])
+  #SNF pipeline:
+  N_df_data_type_1 <- standardNormalization(df_data_type_1)
+  N_df_data_type_2 <- standardNormalization(df_data_type_2)
+  sq_dist_N_df_data_type_1 <- (dist2(as.matrix(N_df_data_type_1), as.matrix(N_df_data_type_1)))^(1/2)
+  sq_dist_N_df_data_type_2 <- (dist2(as.matrix(N_df_data_type_2), as.matrix(N_df_data_type_2)))^(1/2)
+  dist_gower_df_data_type_3 <- daisy(df_data_type_3, metric = c("gower")) 
+  w.data_type_1 <- affinityMatrix(sq_dist_N_df_data_type_1, K=param_combos[i,2], sigma=param_combos[i,3])
+  w.data_type_2 <- affinityMatrix(sq_dist_N_df_data_type_2, K=param_combos[i,2], sigma=param_combos[i,3])
+  w.data_type_3 <- affinityMatrix(as.matrix(dist_gower_df_data_type_3), K=param_combos[i,2], sigma=param_combos[i,3])
+  W_SNF <- SNF(list(w.data_type_1, w.data_type_2, w.data_type_3), K=param_combos[i,2], Tii)
+  estClustNumb <- print(estimateNumberOfClustersGivenGraph(W_SNF, 2:10))
+  estClustNumb_paramCombos[1,i] <- estClustNumb$`Eigen-gap best`
+  estClustNumb_paramCombos[2,i] <- estClustNumb$`Eigen-gap 2nd best`
+  estClustNumb_paramCombos[3,i] <- estClustNumb$`Rotation cost best`
+  estClustNumb_paramCombos[4,i] <- estClustNumb$`Rotation cost 2nd best`
+  rownames(estClustNumb_paramCombos) <- c("Eigengap-best", "Eigengap-2nd-best", "rotation-cost-best", "rotation-cost-2nd-best")
+  print(estClustNumb_paramCombos)
+}
+
+for(i in 1:dim(param_combos)[1]){
+  print(paste("K =", param_combos[i,2], ", alpha =", param_combos[i,3]))
+}
+
+### ==== III. Run SNF bootstrap code - found in seperate script ====
+# === 1. Create 1000 randomly resampled IDs with 80% of subjects 
+df_data_type_1 <- df_ex_func
+bootstrap_80 <- purrr::rerun(1000, resample(rownames(df_data_type_1), size=0.8*(dim(df_data_type_1)[1])))
+save(bootstrap_80, file="bootstrap_80.RData") # copy this R object over to Rosalind
+
+# === 2. Run bootstrap - code for this is in "Hadaya_bootstrap_code.R" - this was run on Rosalind.
+
+#choose the cluster numbers based the cluster numbers that came up most in section (II.)
+
+### ==== IV. MERGE the 1000 outputs that the bootstrap code creates ====
+# copy the outputs from Rosalind to local folder
+
+### ==== i. LOAD and MERGE the iterations ===
+### Groupings: LOAD
+list_ALL_bootstraps_c2 =list_ALL_bootstraps_c3 = list() # list with results from the 10 or 100 or X iterations
+for (i in 1:1000) {
+  list_ALL_bootstraps_c2[[i]] <- as.data.frame(read_excel(paste("run_", i, "param_combo_multi_full_BIG_bootstrap_c2.xlsx", sep ="")))
+  list_ALL_bootstraps_c3[[i]] <- as.data.frame(read_excel(paste("run_", i, "param_combo_multi_full_BIG_bootstrap_c3.xlsx", sep ="")))
+}
+
+### Groupings: MERGE
+#===== for c=2
+multi_full_c2 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_ALL_bootstraps_c2  
+) #this is creating a full/outer join between all elements in the list
+
+
+#===== for c=3
+multi_full_c3 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_ALL_bootstraps_c3 
+) #this is creating a full/outer join between all elements in the list
+
+#===== for c=4
+multi_full_c4 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_ALL_bootstraps_c4
+) #this is creating a full/outer join between all elements in the list
+
+
+### Silh Scores: 
+# LOAD 
+list_ALL_sil_c2 = list_ALL_sil_c3 = list_ALL_sil_c3 = list() # list with results from the 1000 iterations
+for (i in 1:1000) {
+  list_ALL_sil_c2[[i]] <- as.data.frame(read_excel(paste("run_", i, "param_combo_mean_silh_scores_c2.xlsx", sep =""), col_names=FALSE))
+  list_ALL_sil_c3[[i]] <- as.data.frame(read_excel(paste("run_", i, "param_combo_mean_silh_scores_c3.xlsx", sep =""), col_names=FALSE))
+  list_ALL_sil_c4[[i]] <- as.data.frame(read_excel(paste("run_", i, "param_combo_mean_silh_scores_c4.xlsx", sep =""), col_names=FALSE))
+  
+  }
+
+# MERGE: 
+multi_mean_silh_c2 <- unlist(list_ALL_sil_c2)
+multi_mean_silh_c3 <- unlist(list_ALL_sil_c3)
+multi_mean_silh_c4 <- unlist(list_ALL_sil_c4)
+
+### ==== V. SAVE the merged iterations file ====
+
+write.xlsx(multi_full_c2, file = "multi_full_c2.xlsx")
+write.xlsx(multi_full_c3, file = "multi_full_c3.xlsx")
+write.xlsx(multi_full_c4, file = "multi_full_c4.xlsx")
+
+write.xlsx(multi_mean_silh_c2, file = "multi_mean_silh_c2.xlsx")
+write.xlsx(multi_mean_silh_c3, file = "multi_mean_silh_c3.xlsx")
+write.xlsx(multi_mean_silh_c4, file = "multi_mean_silh_c4.xlsx")
+
+### ==== VI. Filter based on silhouette scores: keep highest scoring grouping, from each iteration, only ====
+#data.frame(1st column: number of run, then: 30 rows with the combos, repeated 1000 times, so end up with 30000 rows, 2nd column: index subsample)
+full_silh_data_QC <- data.frame("run_number"=c(1:30000), "seednumber"=c(sort(rep(1:1000,30))), "combo_numb"=rep(1:30, 1000), "K"= rep(param_combos_30[,2], 1000), "alpha"=rep(param_combos_30[,3], 1000), multi_mean_silh_c2, multi_mean_silh_c3, multi_mean_silh_c4)
+
+# 1. look at keeping one top silh per seednumber (end up with 1000)
+keep_highest_only_full_silh_data_QC_c2 <- full_silh_data_QC %>%
+  group_by(seednumber) %>%
+  slice(which.max(multi_mean_silh_c2))
+
+keep_highest_only_full_silh_data_QC_c3 <- full_silh_data_QC %>%
+  group_by(seednumber) %>%
+  slice(which.max(multi_mean_silh_c3))
+
+keep_highest_only_full_silh_data_QC_c4 <- full_silh_data_QC %>%
+  group_by(seednumber) %>%
+  slice(which.max(multi_mean_silh_c4))
+
+# 2. filtered columns to feed into the consensus_combine algorithms
+c2_GROUPS_highest_silh_only <- multi_full_c2[,c(1, keep_highest_only_full_silh_data_QC_c2$run_number+1)]
+c3_GROUPS_highest_silh_only <- multi_full_c3[,c(1, keep_highest_only_full_silh_data_QC_c3$run_number+1)]
+c4_GROUPS_highest_silh_only <- multi_full_c4[,c(1, keep_highest_only_full_silh_data_QC_c4$run_number+1)]
+
+### ==== VII. Apply Consensus Combine and use for stats =======
+rownames(c2_GROUPS_highest_silh_only) <- c2_GROUPS_highest_silh_only$ID
+rownames(c3_GROUPS_highest_silh_only) <- c3_GROUPS_highest_silh_only$ID
+rownames(c4_GROUPS_highest_silh_only) <- c4_GROUPS_highest_silh_only$ID
+
+#c2
+sub1.c2<- as.data.frame(sapply(c2_GROUPS_highest_silh_only[,-1], as.integer))
+sub2.c2<-array(as.matrix(as.data.frame(sub1.c2)),dim=c(198,1000,1,1),dimnames=list(rownames(sub1.c2),colnames(sub1.c2),"SNF","2"))
+cc_c2 <- as.numeric(unlist(consensus_combine(sub2.c2, element = "class")))
+cc_matrix_c2 <- matrix(unlist(consensus_combine(sub2.c2, element = "matrix")), nrow=198, ncol=198)
+
+#c3
+sub1.c3 <- as.data.frame(sapply(c3_GROUPS_highest_silh_only[,-1], as.integer))
+sub2.c3<-array(as.matrix(as.data.frame(sub1.c3)),dim=c(198,1000,1,1),dimnames=list(rownames(sub1.c3),colnames(sub1.c3),"SNF","3"))
+cc_c3 <- as.numeric(unlist(consensus_combine(sub2.c3, element = "class")))
+cc_matrix_c3 <- matrix(unlist(consensus_combine(sub2.c3, element = "matrix")), nrow=198, ncol=198)
+
+#c4
+sub1.c4 <- as.data.frame(sapply(c4_GROUPS_highest_silh_only[,-1], as.integer))
+sub2.c4 <-array(as.matrix(as.data.frame(sub1.c4)),dim=c(198,1000,1,1),dimnames=list(rownames(sub1.c4),colnames(sub1.c4),"SNF","4"))
+cc_c4 <- as.numeric(unlist(consensus_combine(sub2.c4, element = "class")))
+cc_matrix_c4 <- matrix(unlist(consensus_combine(sub2.c4, element = "matrix")), nrow=198, ncol=198)
+
+### ==== VIII. Get Silhouette scores: =======
+
+#c2
+distmatrix=cc_matrix_c2
+sil_CC_c2 <- silhouette_SimilarityMatrix(group = cc_c2 ,similarity_matrix=distmatrix)
+plot(silhouette_SimilarityMatrix(group = cc_c2 ,similarity_matrix=distmatrix))
+plot(sil_CC_c2, do.n.k=F, do.clus.stat=F, col=c("darkolivegreen2","blanchedalmond"))       
+
+#c3
+### --- Here, relabelled cc_c3: 2 as 3 and 3 as 2, because in the manuscript: 
+### --- 1 = resilient group, 2=risk subgroup and 3 - is the NEW emerging (intermediate subgroup)
+cc_c3_relabelled 
+cc_c3_relabelled <- as.numeric(cc_c3_relabelled)
+distmatrix <- cc_matrix_c3
+sil_CC_c3_relabelled <- silhouette_SimilarityMatrix(group = cc_c3_relabelled ,similarity_matrix=distmatrix)
+plot(sil_CC_c3_relabelled, do.n.k=F, do.clus.stat=F, col=c("darkolivegreen2","blanchedalmond", "darksalmon"))       
+
+#c4
+cc_c4_relabelled <- as.numeric(cc_c4_relabelled)
+distmatrix <- cc_matrix_c4
+sil_CC_c4_relabelled <- silhouette_SimilarityMatrix(group = cc_c4_relabelled ,similarity_matrix=distmatrix)
+plot(sil_CC_c4_relabelled, do.n.k=F, do.clus.stat=F, col=c("darkolivegreen2","blanchedalmond", "darksalmon", "cyan4"))       
---
+++ Hadaya_bootstrap_code.R
@@ -0,0 +1,224 @@
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = 
+# Bootstrap code - to run on Rosalind. 
+
+# Study title: "Characterising distinct behavioural subgroups of very preterm born children and 
+#   identifying differences in neonatal structural and functional brain patterns" 
+# Date: 19/11/2021
+# Authors: L Hadaya, K Dimitrakopoulou, L Vanes, D Kanel, S Fenn-Moltu, D Pecheva, G Ball, AD Edwards, 
+# SJ Counsell, M Saqi, D Batalle and C Nosarti
+# = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = = =  
+
+### ==== Install Packages ====
+#install.packages("SNFtool")
+#install.packages("readxl")
+#install.packages("openxlsx")
+#install.packages("BiocManager")
+#BiocManager::install("ConsensusClusterPlus")
+#install.packages("cluster")
+#BiocManager::install("CancerSubtypes")
+#install.packages("purrr")
+#install.packages("ggplot2")
+#install.packages("gdata")
+#install.packages("diceR")
+
+### ==== Load libraries ====
+library(SNFtool)
+library(readxl)
+library(openxlsx)
+library(ConsensusClusterPlus)
+library(cluster)
+library(CancerSubtypes)
+library(data.table)
+library(purrr)
+library(gdata)
+library(ggplot2)
+library(diceR)
+
+### ==== 1. Set things up for Rosalind parallisation =====
+
+args = commandArgs(trailingOnly=TRUE) # This reads input arguments 
+paste0('Seed #', as.numeric(args[1])) # This converts the input to numeric and set as seed
+seednumber = as.numeric(args[1])
+set.seed(seednumber)
+
+#df_data_type_1 <- df_ex_func 
+#bootstrap_80 <- purrr::rerun(1, resample(rownames(df_data_type_1), size=0.8*(dim(df_data_type_1)[1])))
+#bootstrap_80 <- unlist(bootstrap_80)
+
+load("bootstrap_80.RData")
+bootstrap_80 <- unlist(bootstrap_80[[seednumber]])
+
+### ==== 2. Load your data types from your RData.env for the specific run of interest =====
+## !!!! NOTE !!!!!! 
+## make sure data type 3 is the one with categorical data - this data type will have gowers distance in the code below
+
+load("data_types.RData")
+
+df_data_type_1 <-  df_ex_func
+df_data_type_2 <- df_socio_emo
+df_data_type_3 <- as.data.frame(Filter(is.data.frame, mget(ls(pattern="risk$"))))
+print(ls(Filter(is.data.frame, mget(ls(pattern="risk$")))))
+
+### ==== 3. Set param combos =====
+
+N= dim(df_data_type_1)[1]
+N #number of subjects
+parameter_combos_K <- c(rep(10,6), rep(15,6), rep(round((N/10), digits=0),6),rep(25,6), rep(30,6))
+parameter_combos_alpha <- c(rep(c(0.3, 0.4, 0.5, 0.6, 0.7, 0.8),5))
+
+param_combos <- data.frame(c(1:length(parameter_combos_alpha)), parameter_combos_K, parameter_combos_alpha)
+colnames(param_combos) <- c("combo_Num", "K", "alpha")
+
+### ==== 4. Load functions with plot = NULL =====
+
+ExecuteCC2_noPlot <- function (clusterNum, d, maxK = 10, clusterAlg = "hc", distance = "pearson", 
+                               title = "ConsensusClusterResult", reps = 1000, pItem = 0.8, 
+                               pFeature = 1, plot = NULL, innerLinkage = "average", finalLinkage = "average", 
+                               writeTable = FALSE, weightsItem = NULL, weightsFeature = NULL, 
+                               verbose = FALSE, corUse = "everything") 
+{
+  if (is.list(d)) {
+    temp = NULL
+    for (i in 1:length(d)) {
+      temp = rbind(temp, d[[i]])
+    }
+    temp = t(scale(t(temp)))
+  }
+  else temp = d
+  originalResult = ConsensusClusterPlus(temp, maxK = maxK, 
+                                        clusterAlg = clusterAlg, distance = distance, title = title, 
+                                        reps = reps, pItem = pItem, pFeature = pFeature, plot = NULL, 
+                                        innerLinkage = innerLinkage, finalLinkage = finalLinkage, 
+                                        writeTable = writeTable, weightsItem = weightsItem, weightsFeature = weightsFeature, 
+                                        verbose = verbose, corUse = corUse, seed=123456)
+  group = originalResult[[clusterNum]][["consensusClass"]]
+  distanceMatrix = originalResult[[clusterNum]][["consensusMatrix"]]
+  attr(distanceMatrix, "class") = "Similarity"
+  result = list(group = group, distanceMatrix = distanceMatrix, 
+                originalResult = originalResult)
+  result
+}
+
+ExecuteSNF.CC_new_noPlot <- function (W_fused, clusterNum, 
+                                      maxK = 10, pItem = 0.8, reps = 1000, title = "ConsensusClusterResult", 
+                                      plot = NULL, finalLinkage = "average") 
+{
+  
+  W_fused = as.dist(W_fused)
+  result = ExecuteCC2_noPlot(clusterNum = clusterNum, d = W_fused, maxK = maxK, 
+                             clusterAlg = "spectralAlg", title = title, reps = reps, 
+                             pItem = pItem, plot = NULL, finalLinkage = finalLinkage)
+  result
+}
+
+
+### ==== 5. Bootstrap function ====
+
+bootstrap_SNFcc_silh_hyperparam <- function(df_data_type_1, df_data_type_2, df_data_type_3, K, alpha, Tii) {
+  group_SNF_bootstrap_c2 <- list()
+  group_SNF_bootstrap_c3 <- list()
+  group_SNF_bootstrap_c4 <- list()
+  mean_silh_score_c2 <- list()
+  mean_silh_score_c3 <- list()
+  mean_silh_score_c4 <- list()
+  for (j in 1:dim(param_combos)[1]) { 
+    # 1. normalise data frames
+    N_df_data_type_1 <- standardNormalization(df_data_type_1[bootstrap_80,]) # the bootstrap_80[[i]] has a random resampled 80% of the data 
+    N_df_data_type_2 <- standardNormalization(df_data_type_2[bootstrap_80,]) # the bootstrap_80[[i]] has a random resampled 80% of the data 
+    # 2. get distance matrices
+    sq_dist_N_df_data_type_1 <- (dist2(as.matrix(N_df_data_type_1), as.matrix(N_df_data_type_1)))^(1/2)
+    sq_dist_N_df_data_type_2 <- (dist2(as.matrix(N_df_data_type_2), as.matrix(N_df_data_type_2)))^(1/2)
+    dist_gower_df_data_type_3 <- daisy(df_data_type_3[bootstrap_80,], metric = c("gower"))  
+    # 3. get similarity matrices (w)
+    w.data_type_1 <- affinityMatrix(sq_dist_N_df_data_type_1, K=param_combos[j,2], sigma=param_combos[j,3])
+    w.data_type_2 <- affinityMatrix(sq_dist_N_df_data_type_2, K=param_combos[j,2], sigma=param_combos[j,3])
+    w.data_type_3 <- affinityMatrix(as.matrix(dist_gower_df_data_type_3), K=param_combos[j,2], sigma=param_combos[j,3])
+    # 4. get fused W
+    W_SNF <- SNF(list(w.data_type_1, w.data_type_2, w.data_type_3), K=param_combos[j,2], Tii)
+    # 5. get SNF_CC clusters
+    SNF_CC_c2 <- ExecuteSNF.CC_new_noPlot(W_SNF, clusterNum=2, maxK=5, pItem=0.8, reps=1000) 
+    SNF_CC_c3 <- ExecuteSNF.CC_new_noPlot(W_SNF, clusterNum=3, maxK=5, pItem=0.8, reps=1000)
+    SNF_CC_c4 <- ExecuteSNF.CC_new_noPlot(W_SNF, clusterNum=4, maxK=5, pItem=0.8, reps=1000)
+    # 6. get SNF_CC groupings from each iteration  
+    # c2
+    group_SNF_bootstrap_c2[[j]] <- cbind(bootstrap_80, SNF_CC_c2$group) 
+    colnames(group_SNF_bootstrap_c2[[j]]) <- c("ID", "c2")
+    # c3
+    group_SNF_bootstrap_c3[[j]] <- cbind(bootstrap_80, SNF_CC_c3$group) 
+    colnames(group_SNF_bootstrap_c3[[j]]) <- c("ID", "c3")
+    # c4
+    group_SNF_bootstrap_c4[[j]] <- cbind(bootstrap_80, SNF_CC_c4$group) 
+    colnames(group_SNF_bootstrap_c4[[j]]) <- c("ID", "c4")
+    # 7. get silhouette scores from each iteration
+    # 7a. sil score for c2
+    dist_matrix_c2 <- SNF_CC_c2$distanceMatrix
+    silh_scores_from_SNF_bootstrap_c2 <- silhouette_SimilarityMatrix(group = SNF_CC_c2$group, similarity_matrix=dist_matrix_c2)
+    mean_silh_score_c2[[j]] <- mean(silh_scores_from_SNF_bootstrap_c2[,3])
+    # 7b. sil score for c3
+    dist_matrix_c3 <- SNF_CC_c3$distanceMatrix
+    silh_scores_from_SNF_bootstrap_c3 <- silhouette_SimilarityMatrix(group = SNF_CC_c3$group, similarity_matrix=dist_matrix_c3)
+    mean_silh_score_c3[[j]] <- mean(silh_scores_from_SNF_bootstrap_c3[,3])
+    # 7b. sil score for c4
+    dist_matrix_c4 <- SNF_CC_c4$distanceMatrix
+    silh_scores_from_SNF_bootstrap_c4 <- silhouette_SimilarityMatrix(group = SNF_CC_c4$group, similarity_matrix=dist_matrix_c4)
+    mean_silh_score_c4[[j]] <- mean(silh_scores_from_SNF_bootstrap_c4[,3])
+  }
+  # }
+  # 8. Append all outputs to one list 
+  print(c(group_SNF_bootstrap_c2, group_SNF_bootstrap_c3, group_SNF_bootstrap_c4, mean_silh_score_c2, mean_silh_score_c3, mean_silh_score_c4)) # to print out all lists, appended
+  output_list <- list("group_c2"=group_SNF_bootstrap_c2, "group_c3"=group_SNF_bootstrap_c3, "group_c4"=group_SNF_bootstrap_c4, "silh_c2" = mean_silh_score_c2, "silh_c3"=mean_silh_score_c3, "silh_c4"=mean_silh_score_c4)
+  return(output_list)
+}
+### ==== 6. Run the bootstrap ====
+# make sure data type 3 is the one with categorical data - this data type will have gowers dist in the code below
+
+bootstrap_groups_and_silhs = bootstrap_SNFcc_silh_hyperparam(df_data_type_1, df_data_type_2, df_data_type_3, K, alpha, Tii=Tii)
+
+
+### ==== 7. Extract OUTPUT: bootstrap_groups and mean_silh scores ====
+# Extract from the main output (bootstrap_groups_and_silhs), SNF groupings and sil scores: 
+
+list_bootstrap_groups_c2 <- bootstrap_groups_and_silhs$group_c2
+list_bootstrap_groups_c3 <- bootstrap_groups_and_silhs$group_c3
+list_bootstrap_groups_c4 <- bootstrap_groups_and_silhs$group_c4
+
+# turn list into a data.frame - and we are merging by ID number
+#===== for c=2
+multi_full_c2 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_bootstrap_groups_c2
+) 
+
+#===== for c=3
+multi_full_c3 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_bootstrap_groups_c3
+)  
+
+#===== for c=4
+multi_full_c4 <- Reduce(
+  function(x, y, ...) merge(x, y, by="ID", all = TRUE, ...),
+  list_bootstrap_groups_c4
+)  
+
+write.xlsx(multi_full_c2, file=paste("run_", seednumber, "param_combo_multi_full_BIG_bootstrap_c2.xlsx", sep =""))
+write.xlsx(multi_full_c3, file=paste("run_", seednumber, "param_combo_multi_full_BIG_bootstrap_c3.xlsx", sep =""))
+write.xlsx(multi_full_c4, file=paste("run_", seednumber, "param_combo_multi_full_BIG_bootstrap_c4.xlsx", sep =""))
+
+### ==== 8. Extract OUTPUT: mean_silh scores ====
+# Extract from the main output (bootstrap_groups_and_silhs): sil scores
+
+#===== for c=2
+list_mean_silhs_c2 <- bootstrap_groups_and_silhs$silh_c2
+final_mean_silh_c2 <- unlist(list_mean_silhs_c2)
+write.xlsx(final_mean_silh_c2, file=paste("run_", seednumber, "param_combo_mean_silh_scores_c2.xlsx", sep =""))
+
+#===== for c=3
+list_mean_silhs_c3 <- bootstrap_groups_and_silhs$silh_c3
+final_mean_silh_c3 <- unlist(list_mean_silhs_c3)
+write.xlsx(final_mean_silh_c3, file=paste("run_", seednumber, "param_combo_mean_silh_scores_c3.xlsx", sep =""))
+
+#===== for c=4
+list_mean_silhs_c4 <- bootstrap_groups_and_silhs$silh_c4
+final_mean_silh_c4 <- unlist(list_mean_silhs_c4)
+write.xlsx(final_mean_silh_c4, file=paste("run_", seednumber, "param_combo_mean_silh_scores_c4.xlsx", sep =""))
